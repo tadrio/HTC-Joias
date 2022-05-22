1f0926114ae5c565d7f95f7f842a5bd630de69cd
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-05-22 14:29:32 -0300

    styles
    continue the styles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ function constructInfoTop(json) {
   const $wrapper = document.querySelector(".header__info-top__wrapper");
   for (completeText of json) {
     $wrapper.innerHTML += `
-    <span class="header__info-top__wrapper--text">
-      ${completeText.text}
-      <strong class="header__info-top__wrapper--text-bold">
-        ${completeText.bold}
-      </strong>
+    <span class="header__info-top__wrapper${
+      completeText.firstBold ? "--text-bold" : "--text"
+    }">
+      ${completeText.text1}
+      <span class="header__info-top__wrapper${
+        completeText.firstBold ? "--text" : "--text-bold"
+      }">
+        ${completeText.text2}
+      </span>
     </span>`;
   }
 }
